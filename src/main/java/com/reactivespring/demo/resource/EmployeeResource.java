2d18e69a161a22a77bdf2f94472b0ae31d45c48a
saurabh <saurabh945644@gmail.com>
2018-08-18 00:43:26 +0530

    Added Spring 5 Routes

--- src/main/java/com/reactivespring/demo/resource/EmployeeResource.java
+++ src/main/java/com/reactivespring/demo/resource/EmployeeResource.java
@@ -1,8 +1,10 @@
+/*
 package com.reactivespring.demo.resource;
 
 import com.reactivespring.demo.model.Employee;
 import com.reactivespring.demo.model.EmployeeEvent;
 import com.reactivespring.demo.repository.EmployeeRepository;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.util.function.Tuple2;
 
+import java.awt.*;
 import java.time.Duration;
 import java.util.Date;
 import java.util.stream.Stream;
@@ -25,7 +28,7 @@ public class EmployeeResource {
         this.employeeRepository = employeeRepository;
     }
 
-    @GetMapping
+    @GetMapping("/all")
     public Flux<Employee> getAll(){
         return employeeRepository.findAll();
     }
@@ -35,11 +38,11 @@ public class EmployeeResource {
         return employeeRepository.findById(empId);
     }
 
-    @GetMapping("/{id}/events")
+    @GetMapping(value = "/{id}/events", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<EmployeeEvent> getEvents(@PathVariable("id") final String empId) {
         return employeeRepository.findById(empId)
                 .flatMapMany(employee -> {
-                    Flux<Long> interval = Flux.interval(Duration.ofSeconds(5));
+                    Flux<Long> interval = Flux.interval(Duration.ofSeconds(1));
 
                     Flux<EmployeeEvent> employeeEventFlux = Flux.fromStream(
                             Stream.generate(() -> new EmployeeEvent(employee,
@@ -50,3 +53,4 @@ public class EmployeeResource {
                 });
     }
 }
+*/
